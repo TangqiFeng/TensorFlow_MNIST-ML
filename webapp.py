a8660663bbad6caf347eec0fc1468f673167fe57
KyleFeng <kylezete@gmail.com>
2017-11-21 16:28:04 +0000

    add methods to convert image

--- webapp.py
+++ webapp.py
@@ -39,15 +39,48 @@ import base64
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
+        # convert base64 data to image file
+        # adapt from: http://blog.csdn.net/lxdcyh/article/details/4021476
+        # and https://stackoverflow.com/questions/31410525/base64-uri-to-png-python
         image = request.values.get('imageBase64')
         print(image)
         imgdata = base64.b64decode(image.split(",")[1])
+        # here we define the img type: png
+        # and the name of img is image.png 
+        # this means everytime the newest img will replace the old one
+        # 'wb' here means write binary
         with open('uploads/image.png', 'wb') as fh:
             fh.write(imgdata)
+        # get new 28*28 White/Black image
+        reDoImage()
         return ''
         
 
     return render_template("index.html")
+
+# here use Pillow image library
+from PIL import Image
+def reDoImage():
+    # first, read 'image.png' from dolder uploads
+    image = Image.open('uploads/image.png')
+    # Convert png to jpeg type using Pillow 
+    # (from RGBA to RGB)
+    # adapt from https://stackoverflow.com/questions/43258461/convert-png-to-jpeg-using-pillow-in-python
+    bg = Image.new("RGB", image.size, (255,255,255))
+    bg.paste(image,image)
+    bg.save('uploads/image.jpg')
+    resizeImage()
+    
+def resizeImage():
+    # Change the colorful image to black/white image
+    # adapt from: https://stackoverflow.com/questions/18777873/convert-rgb-to-black-or-white
+    bg = Image.open('uploads/image.jpg')
+    gray = bg.convert('L')
+    bw = gray.point(lambda x: 0 if x>128 else 255, '1')
+    # Then, change the image size to 28*28
+    new_image = bw.resize((28, 28))
+    # save image.jpg
+    new_image.save('uploads/image_28.jpg')
 
 
 @app.route('/uploaded_file')
